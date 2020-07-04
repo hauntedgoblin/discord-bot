ad9ad0ed556f8576aafd6c449963d2753baecb49
Cameron <cameronmunson12@gmail.com>
2020-07-03 23:13:05 -0500

    ping message update

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,7 @@ module.exports = {
     execute(message, args, client, prefix) {
         let msg = new Discord.MessageEmbed()
             .setColor('#0099FF')
-            .setDescription('pong! :)');
+            .setDescription('pong!');
         message.channel.send(msg);
     },
 };
