59b4dd7e0b9b8f6c51604a0954d8368d1d23bc26
Cameron <cameronmunson12@gmail.com>
2020-07-03 23:10:50 -0500

    ping fix

--- commands/ping.js
+++ commands/ping.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-const Discord = require('discord.js');
-
-module.exports = {
-    name: 'ping',
-    description: 'Ping the bot.',
-    usage: ' ',
-    cooldown: 60,
-    guildOnly: false,
-    execute(message, args, client, prefix) {
-        let msg = new Discord.MessageEmbed()
-            .setColor('#0099FF')
-            .setDescription('pong!');
-        message.channel.send(msg);
-    },
-};
-=======
 const Discord = require('discord.js');
 
 module.exports = {
@@ -30,4 +13,3 @@ module.exports = {
         message.channel.send(msg);
     },
 };
->>>>>>> c773c075cc9df7fa17a87d1a40866a47f98a8d7e
