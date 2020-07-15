c0aecb67d7223c0bd615fa2abe4002e9dd0e2650
Cameron <cameronmunson12@gmail.com>
2020-07-14 22:59:10 -0500

    update embed command restrictions

--- commands/embed.js
+++ commands/embed.js
@@ -11,6 +11,15 @@ module.exports = {
 
         let msg = new Discord.MessageEmbed().setColor('#0099FF');
 
+        if (message.author.id !== '213814266912964610') {
+            msg
+                .setDescription(`${message.author}, you are not authorized to use that command.`);
+            message.channel.send(msg)
+                .then(m => { m.delete({ timeout: 2500 }) });
+            message.delete();
+            return;
+        };
+
         msg
             .setTitle('Embed title')
             .setTimestamp()
